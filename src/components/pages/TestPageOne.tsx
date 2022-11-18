6215d12756bd986b7ce165b9923ad7fa04815506
Folasayo O. Akinyosoye <fakinyosoye@boi.ng>
2022-11-18 11:30:05 +0100

    worked on input field component

--- src/components/pages/TestPageOne.tsx
+++ src/components/pages/TestPageOne.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Input } from "../atoms";
 
 const TestPageOne = () => {
   return (
-    <div>
-      <p>TestPageOne</p>
+    <div className="mt-8 mx-10">
+      <Input type="text" />
     </div>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "rgba(7, 199, 39, 0.7)",
+    },
   },
   plugins: [],
 };
