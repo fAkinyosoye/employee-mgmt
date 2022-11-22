5c9ea68bb351f75caa023f420faa10b7a8868619
Folasayo O. Akinyosoye <fakinyosoye@boi.ng>
2022-11-22 13:37:56 +0100

    resolved merge conflict

--- src/components/pages/EmployeeDetails.tsx
+++ src/components/pages/EmployeeDetails.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-<<<<<<< HEAD
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import {
@@ -9,30 +8,17 @@ import {
   useFetchAllGradeLevelsQuery,
   useFetchBOIEmployeeByIdQuery,
 } from "../../redux/services/mgmt-services";
-=======
-import { useLocation, useNavigate } from "react-router-dom";
->>>>>>> 9df0f5c316b4b0d5a4d920066a785ad24fef5401
 import {
   Button,
   CustomSelect,
   Header1,
   Input,
-<<<<<<< HEAD
   Loader,
-=======
-  Label,
->>>>>>> 9df0f5c316b4b0d5a4d920066a785ad24fef5401
   Subtitle,
 } from "../atoms";
 
 const EmployeeDetails = () => {
-<<<<<<< HEAD
   const { id } = useParams();
-=======
-  const navigate = useNavigate();
-
-  const { state } = useLocation();
->>>>>>> 9df0f5c316b4b0d5a4d920066a785ad24fef5401
 
   let employeeID = id ?? "";
 
