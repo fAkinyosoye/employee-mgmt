57c737c6534561f0353fd86ca7a5eaabdf6571f4
Folasayo O. Akinyosoye <fakinyosoye@boi.ng>
2022-11-28 08:45:20 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8222,
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 });
